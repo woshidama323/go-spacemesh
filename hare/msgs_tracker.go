4749bf93bfe85f8e2cf20b2a012e6ed7fad255aa
Piers Powlesland <pierspowlesland@gmail.com>
2023-04-03 16:05:51 +0100

    Revert "Extract interface for message tracking"
    This reverts commit d7e2a6b0e3a1881277e2dd5063bd37d7bc25b7e4.

--- hare/msgs_tracker.go
+++ hare/msgs_tracker.go
@@ -4,23 +4,18 @@ import (
 	"github.com/spacemeshos/go-spacemesh/common/types"
 )
 
-type msgsTracker interface {
-	Track(*Msg)
-	NodeID(*Message) types.NodeID
-}
-
-type defaultMsgsTracker struct {
+type msgsTracker struct {
 	sigToPub map[types.EdSignature]types.NodeID
 }
 
-func (mt *defaultMsgsTracker) Track(m *Msg) {
+func (mt *msgsTracker) Track(m *Msg) {
 	mt.sigToPub[m.Signature] = m.NodeID
 }
 
-func (mt *defaultMsgsTracker) NodeID(m *Message) types.NodeID {
+func (mt *msgsTracker) NodeID(m *Message) types.NodeID {
 	return mt.sigToPub[m.Signature]
 }
 
-func newMsgsTracker() *defaultMsgsTracker {
-	return &defaultMsgsTracker{sigToPub: make(map[types.EdSignature]types.NodeID)}
+func newMsgsTracker() *msgsTracker {
+	return &msgsTracker{sigToPub: make(map[types.EdSignature]types.NodeID)}
 }
